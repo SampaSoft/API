671879e124e6b454aa6c0981046e89c88786a835
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-06-16 12:27:47 -0300

    Commit SQL Secretarias
    Script com a criação da tabela de scretarias.

---
+++ SampaSoft - API/SampaSoft.Infraestrutura/SQL/SQL1.0.0.4.sql
@@ -0,0 +1,35 @@
+﻿USE [SampasoftDB]
+GO
+
+/****** Object:  Table [dbo].[tbSecretarias]    Script Date: 16/06/2018 12:26:38 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbSecretarias](
+	[codSecretaria] [int] NOT NULL,
+	[cnpj] [nvarchar](50) NULL,
+	[nome] [nvarchar](250) NULL,
+	[email] [nvarchar](50) NULL,
+	[logradouro] [nvarchar](250) NULL,
+	[numero] [nvarchar](50) NULL,
+	[complemento] [nvarchar](50) NULL,
+	[codMunicipio] [int] NULL,
+	[telefone] [nvarchar](50) NULL,
+	[horaAtendimentoInicial] [time](7) NULL,
+	[horaAtendimentoFinal] [time](7) NULL,
+	[ativo] [bit] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[tbSecretarias] ADD  CONSTRAINT [DF_tbSecretarias_ativo]  DEFAULT ((1)) FOR [ativo]
+GO
+
+ALTER TABLE [dbo].[tbSecretarias]  WITH CHECK ADD  CONSTRAINT [FK_tbSecretarias_tbMunicipios] FOREIGN KEY([codMunicipio])
+REFERENCES [dbo].[tbMunicipios] ([codMunicipio])
+GO
+
+ALTER TABLE [dbo].[tbSecretarias] CHECK CONSTRAINT [FK_tbSecretarias_tbMunicipios]
