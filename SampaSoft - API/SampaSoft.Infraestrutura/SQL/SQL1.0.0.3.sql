4af16fac7f6eb014b2a844af7ff1f1a8c3f0f341
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-06-16 12:07:53 -0300

    Alteração Municipios
    Adicionado Chave Primária nos Municipios.

--- SampaSoft - API/SampaSoft.Infraestrutura/SQL/SQL1.0.0.3.sql
+++ SampaSoft - API/SampaSoft.Infraestrutura/SQL/SQL1.0.0.3.sql
@@ -1,7 +1,7 @@
 ﻿USE [SampasoftDB]
 GO
 
-/****** Object:  Table [dbo].[tbMunicipios]    Script Date: 16/06/2018 12:05:15 ******/
+/****** Object:  Table [dbo].[tbMunicipios]    Script Date: 16/06/2018 12:07:25 ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,7 +9,11 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE TABLE [dbo].[tbMunicipios](
-	[codMunicipio] [int] NULL,
+	[codMunicipio] [int] IDENTITY(1,1) NOT NULL,
 	[nome] [nvarchar](250) NULL,
-	[codIBGE] [int] NULL
+	[codIBGE] [int] NULL,
+ CONSTRAINT [PK_tbMunicipios] PRIMARY KEY CLUSTERED 
+(
+	[codMunicipio] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
