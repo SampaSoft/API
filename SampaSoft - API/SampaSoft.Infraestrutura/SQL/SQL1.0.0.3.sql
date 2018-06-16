50a12eb9842c266a83eeeb21b12d3970960176d1
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-06-16 12:05:57 -0300

    SQL Municipios
    Script SQL para a criação de Municipios

---
+++ SampaSoft - API/SampaSoft.Infraestrutura/SQL/SQL1.0.0.3.sql
@@ -0,0 +1,15 @@
+﻿USE [SampasoftDB]
+GO
+
+/****** Object:  Table [dbo].[tbMunicipios]    Script Date: 16/06/2018 12:05:15 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbMunicipios](
+	[codMunicipio] [int] NULL,
+	[nome] [nvarchar](250) NULL,
+	[codIBGE] [int] NULL
+) ON [PRIMARY]
