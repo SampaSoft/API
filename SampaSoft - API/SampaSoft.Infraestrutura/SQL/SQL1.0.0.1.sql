76d80f62f02fcfe6f4a8025b0dc7fb50dbd10b56
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-06-16 11:38:30 -0300

    Script SQL
    Script SQL com a criação da tabela de usuários

---
+++ SampaSoft - API/SampaSoft.Infraestrutura/SQL/SQL1.0.0.1.sql
@@ -0,0 +1,39 @@
+﻿[SampasoftDB]
+GO
+
+/****** Object:  Table [dbo].[tbUsuarios]    Script Date: 16/06/2018 11:37:40 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbUsuarios](
+	[codUsuario] [int] IDENTITY(1,1) NOT NULL,
+	[nome] [nvarchar](250) NULL,
+	[rg] [nvarchar](50) NULL,
+	[cpf] [nvarchar](50) NULL,
+	[logradouro] [nvarchar](250) NULL,
+	[numero] [nvarchar](50) NULL,
+	[complemento] [nvarchar](250) NULL,
+	[bairro] [nvarchar](50) NULL,
+	[cidade] [nvarchar](50) NULL,
+	[estado] [char](2) NULL,
+	[cep] [nvarchar](50) NULL,
+	[sexo] [char](1) NULL,
+	[dataNascimento] [datetime] NULL,
+	[senha] [nvarchar](max) NULL,
+	[dataCadastro] [datetime] NULL,
+	[ativo] [bit] NULL,
+ CONSTRAINT [PK_tbUsuarios] PRIMARY KEY CLUSTERED 
+(
+	[codUsuario] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[tbUsuarios] ADD  CONSTRAINT [DF_tbUsuarios_dataCadastro]  DEFAULT (getdate()) FOR [dataCadastro]
+GO
+
+ALTER TABLE [dbo].[tbUsuarios] ADD  CONSTRAINT [DF_tbUsuarios_ativo]  DEFAULT ((1)) FOR [ativo]
