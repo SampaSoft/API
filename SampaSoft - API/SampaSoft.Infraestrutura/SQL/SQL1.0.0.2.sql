d9a1b9742198cc6e52de658bce95c775a319ed39
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-06-16 11:57:07 -0300

    SQL Fornecedores
    Scritpt SQL com a tabela de fornecedores.

---
+++ SampaSoft - API/SampaSoft.Infraestrutura/SQL/SQL1.0.0.2.sql
@@ -0,0 +1,32 @@
+﻿USE [SampasoftDB]
+GO
+
+/****** Object:  Table [dbo].[tbFornecedores]    Script Date: 16/06/2018 11:55:57 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbFornecedores](
+	[codFornecedor] [int] NOT NULL,
+	[cpfcnpj] [nvarchar](50) NULL,
+	[razaoSocial] [nvarchar](50) NULL,
+	[nomeFantasia] [nvarchar](50) NULL,
+	[logradouro] [nvarchar](250) NULL,
+	[numero] [nvarchar](50) NULL,
+	[complemento] [nvarchar](250) NULL,
+	[bairro] [nvarchar](50) NULL,
+	[cidade] [nvarchar](50) NULL,
+	[estado] [char](2) NULL,
+	[cep] [nvarchar](50) NULL,
+	[dataCadastro] [datetime] NULL,
+	[ativo] [bit] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [dbo].[tbFornecedores] ADD  CONSTRAINT [DF_tbFornecedores_dataCadastro]  DEFAULT (getdate()) FOR [dataCadastro]
+GO
+
+ALTER TABLE [dbo].[tbFornecedores] ADD  CONSTRAINT [DF_tbFornecedores_ativo]  DEFAULT ((1)) FOR [ativo]
