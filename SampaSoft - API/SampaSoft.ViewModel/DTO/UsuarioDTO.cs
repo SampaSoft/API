d524554a346ea98b117545117e71f9ec559a1124
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-06-16 23:42:06 -0300

    add ViewModel Project
    Adicionado Projeto de ViewModel responsável por conter as classes de DTO (Data Transfer Object)

---
+++ SampaSoft - API/SampaSoft.ViewModel/DTO/UsuarioDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SampaSoft.ViewModel.DTO
+{
+    public class UsuarioDTO
+    {
+        public string codUsuario { get; set; }
+        public string nome { get; set; }
+        public string usuario { get; set; }
+        public DateTime dataCadastro { get; set; }
+        public bool ativo { get; set; }
+    }
+}
