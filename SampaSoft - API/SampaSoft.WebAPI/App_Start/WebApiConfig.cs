98b65ac4e373de826551fe93615589bf19d89817
Marcelo Barbosa <ma.barbosa@outlook.com>
2018-06-17 00:05:38 -0300

    Core Injeção de Dependência
    Adicionado as classes que configuram a injeção de dependência.

--- SampaSoft - API/SampaSoft.WebAPI/App_Start/WebApiConfig.cs
+++ SampaSoft - API/SampaSoft.WebAPI/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 using Microsoft.Practices.Unity;
-using SampaSoft.ViewModel.DTO;
+using SampaSoft.WebAPI.DTO;
 
 namespace SampaSoft.WebAPI
 {
